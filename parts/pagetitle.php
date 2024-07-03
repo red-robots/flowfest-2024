aa52f03d7762909dc62d7b7315ceb6b4b5d853f6
accrane <austincrane@gmail.com>
2024-07-03 14:30:53 -0400

    remove reg on page

--- parts/pagetitle.php
+++ parts/pagetitle.php
@@ -6,9 +6,11 @@ $register_button_target = (isset($register['target']) && $register['target']) ? 
 <section class="pagetitle">
 	<div class="wrapper pagetitle-flex">
   <?php if($register_button_text && $register_button_link) { ?>
- 	<div class="reg">
- 		<a href="<?php echo $register_button_link; ?>" target="<?php echo $register_button_target; ?>"><span><?php echo $register_button_text; ?></span></a>
- 	</div>
+ 	<?php if( !is_page(1422) ) { ?>
+	 	<div class="reg">
+	 		<a href="<?php echo $register_button_link; ?>" target="<?php echo $register_button_target; ?>"><span><?php echo $register_button_text; ?></span></a>
+	 	</div>
+	 <?php } ?>
   <?php } ?>
   <?php if( !is_front_page() ) { ?>
  	<h1 class="page-title">
